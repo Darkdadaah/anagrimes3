2b5f576aa1fc75598cdcace9fddf33793dcc96db
Darkdadaah <darkdadaah@yahoo.fr>
2020-05-09 15:05:10 +0100

    Further parse, output json

--- lib/wikt/article.py
+++ lib/wikt/article.py
@@ -6,10 +6,10 @@ class Article:
     
     def __init__(self, title, text):
         self.title = title
-        self.words = self.parse(text)
+        self.words = self.parse(title, text)
 
-    def parse(self, text):
-        words = {}
+    def parse(self, title, text):
+        words = []
 
         # Parse language sections
         lang = ""
@@ -27,6 +27,12 @@ class Article:
                     wlang = word_match.group(2)
                     self.log("Word = " + wtype)
 
+                    if cur_word:
+                        words.append(cur_word)
+                        cur_word = None
+
+                    cur_word = Word(title, lang, wtype)
+
                     if wlang != lang:
                         self.log("Langue different in word section: %s vs %s " % (lang, wlang))
                 #else:
@@ -35,12 +41,52 @@ class Article:
                 form_line = line
                 self.log("Got form line")
             elif line.startswith("#"):
-                def_line = "line"
-                self.log("Got def")
+                if def_match := re.search("^#+([^#*:] *.+)$", line):
+                    def_line = def_match.group(1)
+                    cur_word.add_def(def_line.strip())
 
         return words
 
-    def log(self, text):
-        print("LOG: " + text)
+    def __str__(self):
+        lines = [
+                "TITLE = %s" % self.title,
+                "WORDS = %d" % len(self.words)
+                ]
+        return "\n".join(lines)
 
+
+    def log(self, text):
+        pass
+        #print("LOG: " + text)
+
+
+class Word:
+
+    def __init__(self, title, lang, wtype):
+        self.title = title
+        self.lang = lang
+        self.type = wtype
+        self.form = None
+        self.defs = []
+
+    def add_def(self, def_line):
+        self.defs.append(def_line)
+
+    def __str__(self):
+        lines = [
+                "TITLE = %s" % self.title,
+                "LANG  = %s" % self.lang,
+                "TYPE  = %s" % self.type,
+                "DEFS  = %d" % len(self.defs)
+                ]
+        return "\n\t".join(lines)
+
+    def struct(self):
+        struct = {
+            "title" : self.title,
+            "lang" : self.lang,
+            "type" : self.type,
+            "defs" : self.defs,
+        }
+        return struct
 
--- scripts/test.py
+++ scripts/test.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 
 import os
+import json
 from wikt.article import Article
+
 
 test_file = os.path.join(os.path.dirname(__file__), "../data/coin.txt")
 title = "coin"
@@ -10,4 +12,14 @@ with open(test_file, "r") as f:
 
 a = Article(title, text)
 print(a.title)
-#print(a.text)
+
+print(a)
+for word in a.words:
+    print(word)
+
+# Print to json file
+out_file = os.path.join(os.path.dirname(__file__), "../data/coin.json")
+
+struct = [w.struct() for w in a.words]
+with open(out_file, "w") as outf:
+     outf.write(json.dumps(struct, indent=4, ensure_ascii=False))

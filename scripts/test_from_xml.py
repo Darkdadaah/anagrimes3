67d19ef2755b4db7549395f8c3ced109e80ec3a9
Darkdadaah <darkdadaah@yahoo.fr>
2020-05-09 19:41:43 +0100

    Improve script, better mem management

--- scripts/test_from_xml.py
+++ scripts/test_from_xml.py
@@ -3,19 +3,22 @@
 import os
 import json
 from wikt.article import Article
-import xml.etree.ElementTree as ET
+#import xml.etree.ElementTree as ET
+from lxml import etree as ET
 import re
 
 
 xml_file = os.path.join(os.path.dirname(__file__), "../data/frwikt.xml")
 
 # Print to json file
-out_file = os.path.join(os.path.dirname(__file__), "../data/frwikt.jsonl")
+out_file = os.path.join(os.path.dirname(__file__), "../data/frwikt_2.json")
 
 with open(out_file, "w") as outf:
     title = None
+    num = 0
     namespaced = re.compile(".+:.+")
-    for event, elem in ET.iterparse(xml_file, events=("start","end")):
+    context = ET.iterparse(xml_file, events=("start","end"))
+    for event, elem in context:
         _, _, tag = elem.tag.rpartition('}')
         if tag == "title" and event == "end":
             if namespaced.match(elem.text):
@@ -23,9 +26,22 @@ with open(out_file, "w") as outf:
             title = elem.text
             elem.clear()
         elif title != None and tag == "text" and event == "end":
+            num += 1
+            if num % 100000 == 0:
+                print("%d articles" % num)
             article = Article(title, elem.text)
             for word in article.words:
                 outf.write(json.dumps(word.struct(), ensure_ascii=False, indent=4) + "\n")
             title = None
-            elem.clear()
+            
+            # Cleanup, from https://www.ibm.com/developerworks/xml/library/x-hiperfparse/
+#            elem.clear()
+#            while elem.getprevious() is not None:
+#                del elem.getparent()[0]
+
+
+            for ancestor in elem.xpath('ancestor-or-self::*'):
+                while ancestor.getprevious() is not None:
+                    del ancestor.getparent()[0]
+    del context
 

b2f3ab73c4be4169d7491414114d1d2234a8269d
Darkdadaah <darkdadaah@yahoo.fr>
2025-09-30 17:57:50 +0100

    Separate function, more comments

--- lib/wikt/xml_splitter.py
+++ lib/wikt/xml_splitter.py
@@ -1,9 +1,12 @@
 """Parse a Wiktionnaire xml dump into a jsonl format."""
 
 import logging
+from pathlib import Path
 
 import argparse
 from lxml import etree
+
+DEFAULT_BATCH = 10000
 
 
 def main():
@@ -11,7 +14,7 @@ def main():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("input", type=str, help="xml dump path")
     parser.add_argument("output", type=str, help="json output basename")
-    parser.add_argument("batch", type=int, help="number of articles per batch file", default=10000)
+    parser.add_argument("batch", type=int, help="number of articles per batch file", default=DEFAULT_BATCH)
     parser.add_argument(
         "-v",
         "--verbose",
@@ -21,11 +24,12 @@ def main():
     )
     args = parser.parse_args()
     logging.basicConfig(level=args.loglevel)
+    split_xml(Path(args.input), Path(args.output),args.batch)
 
-    xml_file = args.input
-    output = args.output
-    batch_size = args.batch
-
+def split_xml(xml_file: Path, output: Path, batch_size: int = DEFAULT_BATCH) -> None:
+    """Split a Mediawiki xml dump into smaller files.
+    """
+    # Define Namespace to keep: only main articles
     xml_ns = "{http://www.mediawiki.org/xml/export-0.11/}"
     ns = 0
 
@@ -43,23 +47,29 @@ def main():
     outf = open(out_file, "wb") # pylint: disable=consider-using-with
     outf.write(xml_head)
 
-    num = 0
-    skipped = 0
+    # Keep some stats
+    num_articles = 0
+    num_skipped = 0
+
+    # Parse XML
     context = etree.iterparse(xml_file, events=("start", "end"))
     for event, elem in context:
         _, _, tag = elem.tag.rpartition("}")
 
         if tag == "page" and event == "end":
-            page_ns = int(elem.find(xml_ns + "ns").text)
+            page_ns = int(elem.find(f"{xml_ns}ns").text)
             if page_ns != ns:
-                skipped += 1
+                num_skipped += 1
                 continue
 
-            num += 1
-            if num % batch_size == 0:
-                logging.info(f"{num} articles to {out_file}")
+            num_articles += 1
+            if num_articles % batch_size == 0:
+                # Close current file
+                logging.info(f"{num_articles} articles to {out_file}")
                 outf.write(xml_foot)
                 outf.close()
+
+                # Prep next file
                 num_file += 1
                 out_file = f"{output}_{num_file}.xml"
                 outf = open(out_file, "wb") # pylint: disable=consider-using-with
@@ -71,12 +81,12 @@ def main():
             for ancestor in elem.xpath("ancestor-or-self::*"):
                 while ancestor.getprevious() is not None:
                     del ancestor.getparent()[0]
-
-    del context
     outf.write(xml_foot)
     outf.close()
-    logging.info(f"{num} pages parsed")
-    logging.info(f"{num} pages skipped")
+    del context
+
+    logging.info(f"{num_articles} pages parsed")
+    logging.info(f"{num_articles} pages skipped")
 
 
 if __name__ == "__main__":

8a8f8d405551be430e8579f1d79b66dc0b6b14ff
Darkdadaah <darkdadaah@hotmail.com>
2026-03-22 12:10:13 +0100

    Better doc for xml

--- lib/wikt/xml.py
+++ lib/wikt/xml.py
@@ -1,4 +1,4 @@
-"""Tools to parse a Mediawiki XML dump."""
+"""Tools to parse a Mediawiki XML dump into individual page elements."""
 
 from pathlib import Path
 from typing import Generator
@@ -9,10 +9,20 @@ from lxml import etree
 # Only return pages from the Main namespace by default
 _ALLOWED_NS = (0,)
 _XML_NS = "{http://www.mediawiki.org/xml/export-0.11/}ns"
+def get_pages(xml_file: Path, allowed_ns: tuple[int] = _ALLOWED_NS) -> Generator[etree.Element, None, None]:
+    """Generator for mediawiki XML pages from an XML dump.
 
+    This function parses a Mediawiki XML dump and yields individual page elements
+    that belong to the specified namespaces. By default, it only returns pages from
+    the Main namespace (namespace ID 0).
 
-def get_pages(xml_file: Path, allowed_ns: tuple[int] = _ALLOWED_NS)-> Generator[etree.Element, None, None]:
-    """Generator for mediawiki pages given an XML dump."""
+    Parameters:
+        xml_file (Path): The path to the Mediawiki XML dump file.
+        allowed_ns (tuple[int]): A tuple of namespace IDs to include in the output.
+
+    Yields:
+        etree.Element: An XML element representing a Mediawiki page.
+    """
 
     # Parse XML
     context = etree.iterparse(xml_file, events=("start", "end"))
